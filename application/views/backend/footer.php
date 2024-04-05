e791c4ac1cf0100d0055b3b8733a4801d580fd52
Rahul Prajapati <rahul@sixsenseinfo.com>
2024-04-05 12:57:08 +0530

    My NIFL Project setup

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | National Institute Of Foreigner Languages (NIFL). php using CodeIgniter Web Framework |
+    <strong>Developed by MR. Rahul Prajapati, jiwaji university.</strong>
+</footer>
